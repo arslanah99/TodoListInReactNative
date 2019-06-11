c07fe4f8459e1259cc00d772e2a886001e4998ef
arslanah99 <Arah09@hotmail.com>
2019-06-11 13:49:41 -0600

    Created first componenet

--- AwesomePRoject/App.js
+++ AwesomePRoject/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
+      <Hello />
     </View>
   );
 }
