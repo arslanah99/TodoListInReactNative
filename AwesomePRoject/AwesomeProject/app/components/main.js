a766231834d0a3476df53c7db3c693d646635039
arslanah99 <Arah09@hotmail.com>
2019-06-11 16:05:04 -0600

    formated project

--- AwesomePRoject/AwesomeProject/app/components/main.js
+++ AwesomePRoject/AwesomeProject/app/components/main.js
@@ -1,121 +1,132 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
-import Note from './note'
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
+import Note from "./note";
 
 export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteArray: [],
+      noteText: ""
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            noteArray: [],
-            noteText: ''
-        }
+  render() {
+    let notes = this.state.noteArray.map((val, key) => {
+      return (
+        <Note
+          key={key}
+          keyval={key}
+          val={val}
+          deleteMethod={() => this.deleteNote(key)}
+        />
+      );
+    });
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}> ISSA TODO </Text>
+        </View>
+
+        <ScrollView style={styles.ScrollView}>{notes}</ScrollView>
+
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={noteText => this.setState({ noteText })}
+            value={this.state.noteText}
+            placeholder=">Create a todo"
+            placeholderTextColor="white"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+
+        <TouchableOpacity
+          onPress={this.addNote.bind(this)}
+          style={styles.addButton}
+        >
+          <Text style={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  addNote() {
+    if (this.state.noteText) {
+      var d = new Date();
+      this.state.noteArray.push({
+        date: d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDay(),
+        note: this.state.noteText
+      });
+      this.setState({ noteArray: this.state.noteArray });
+      this.setState({ noteText: "" });
     }
+  }
 
-    render(){
-
-        let notes = this.state.noteArray.map((val, key) => {
-            return <Note key={key} keyval={key} val={val} deleteMethod={() => this.deleteNote(key)} />
-        })
-
-        return (
-            <View style={styles.container}>
-                
-                <View style={styles.header}>
-                    <Text style={styles.headerText}> ISSA TODO </Text>
-                </View>
-
-                <ScrollView style={styles.ScrollView}>
-                    {notes}
-                </ScrollView>
-
-                <View style={styles.footer}>
-
-                    <TextInput style={styles.textInput}
-                    onChangeText={(noteText) => this.setState({noteText})}
-                    value={this.state.noteText}
-                    placeholder='>Create a todo'
-                    placeholderTextColor='white'
-                    underlineColorAndroid='transparent'>
-                    </TextInput>
-
-                </View>
-
-                <TouchableOpacity onPress={this.addNote.bind(this)} style={styles.addButton}>
-                    <Text style={styles.addButtonText}>+</Text>
-                </TouchableOpacity>
-
-            </View>
-        );
-    }
-    addNote() {
-        if (this.state.noteText){
-            var d = new Date();
-            this.state.noteArray.push({
-                'date': d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDay(), 'note': this.state.noteText
-            });
-            this.setState({ noteArray: this.state.noteArray })
-            this.setState({ noteText: ""});
-        }
-    }
-
-    deleteNote(key){
-        this.state.noteArray.splice(key, 1);
-        this.setState({ noteArray: this.state.noteArray })
-    }
+  deleteNote(key) {
+    this.state.noteArray.splice(key, 1);
+    this.setState({ noteArray: this.state.noteArray });
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   header: {
-      backgroundColor: '#E91E63',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderBottomWidth: 10,
-      borderBottomColor: '#ddd'
+    backgroundColor: "#E91E63",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 10,
+    borderBottomColor: "#ddd"
   },
   headerText: {
-      color: 'white',
-      fontSize: 18,
-      padding: 26,
+    color: "white",
+    fontSize: 18,
+    padding: 26
   },
   scrollContainer: {
-      flex: 1,
-      marginBottom: 100,
+    flex: 1,
+    marginBottom: 100
   },
   footer: {
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      zIndex: 10,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10
   },
   textInput: {
-      alignSelf: 'stretch',
-      color: '#fff',
-      padding: 20,
-      backgroundColor: '#252525',
-      borderTopWidth: 2,
-      borderBottomColor: '#ededed',
+    alignSelf: "stretch",
+    color: "#fff",
+    padding: 20,
+    backgroundColor: "#252525",
+    borderTopWidth: 2,
+    borderBottomColor: "#ededed"
   },
 
   addButton: {
-      position: 'absolute',
-      zIndex: 11,
-      right: 20,
-      bottom: 90,
-      backgroundColor: '#E91E63',
-      width: 90,
-      height: 90,
-      borderRadius: 50,
-      alignItems: 'center',
-      justifyContent: 'center',
-      elevation: 8,
+    position: "absolute",
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: "#E91E63",
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 8
   },
   addButtonText: {
-      color: '#fff',
-      fontSize: 24,
+    color: "#fff",
+    fontSize: 24
   }
 });
